a26dbbc7fe94e80921b727e661601c6df89c3afd
Kaung722 <70074279+Kaung722@users.noreply.github.com>
2021-07-18 10:35:05 -0700

    fixed submit problem

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log(lee);
+
+//Selector
+const nameInput = document.querySelector("#nameInput");
+const dateInput = document.querySelector("#dateInput");
+const amountInput = document.querySelector("#amountInput");
+const addExpenseBtn = document.querySelector("#add_expense_button");
+const form = document.querySelector("#formID");
+
+//Listener
+// addExpenseBtn.addEventListener("click", function () {
+//   console.log("Lee Zwe");
+// });
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(nameInput.value, dateInput.value, amountInput.value);
+});
