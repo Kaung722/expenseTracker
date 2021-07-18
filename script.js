cfb36f01dee587ebb43f68b087c7fd70d050c22a
Kaung722 <70074279+Kaung722@users.noreply.github.com>
2021-07-18 12:31:32 -0700

    added row-adding function

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ const dateInput = document.querySelector("#dateInput");
 const amountInput = document.querySelector("#amountInput");
 const addExpenseBtn = document.querySelector("#add_expense_button");
 const form = document.querySelector("#formID");
+let expenseTable = document.querySelector(".expense_table");
 
 //Listener
-// addExpenseBtn.addEventListener("click", function () {
-//   console.log("Lee Zwe");
-// });
-
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(nameInput.value, dateInput.value, amountInput.value);
+
+  let nameCell = document.createElement("td");
+  let dateCell = document.createElement("td");
+  let amountCell = document.createElement("td");
+  let deleteCell = document.createElement("button");
+  nameCell.appendChild(document.createTextNode(nameInput.value));
+  dateCell.appendChild(document.createTextNode(dateInput.value));
+  amountCell.appendChild(document.createTextNode(amountInput.value));
+  deleteCell.appendChild(document.createTextNode("❌"));
+  let newRow = document.createElement("tr");
+  newRow.appendChild(nameCell);
+  newRow.appendChild(dateCell);
+  newRow.appendChild(amountCell);
+  newRow.appendChild(deleteCell);
+  expenseTable.appendChild(newRow);
 });
