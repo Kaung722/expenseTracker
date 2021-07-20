df9123193f1ba8ac9d77e5a0a7efa021c2881889
Nay Toe <ksithu722@gmail.com>
2021-07-19 23:52:36 -0700

    change code

--- script.js
+++ script.js
@@ -17,23 +17,31 @@ expenseTable.addEventListener("click", removeExpense);
 function addExpense(event) {
   event.preventDefault();
   console.log(nameInput.value, dateInput.value, amountInput.value);
-  let nameCell = document.createElement("td");
-  let dateCell = document.createElement("td");
-  let amountCell = document.createElement("td");
-  let deleteCell = document.createElement("button");
-  nameCell.appendChild(document.createTextNode(nameInput.value));
-  dateCell.appendChild(document.createTextNode(dateInput.value));
-  amountCell.appendChild(document.createTextNode(amountInput.value));
-  deleteCell.appendChild(document.createTextNode("❌"));
-  deleteCell.classList.add("delete_btn");
-  let newRow = document.createElement("tr");
-  newRow.appendChild(nameCell);
-  newRow.appendChild(dateCell);
-  newRow.appendChild(amountCell);
-  newRow.appendChild(deleteCell);
-  expenseTable.appendChild(newRow);
+  const newExpense = `
+  <tr>
+          <td class="output">${nameInput.value}</td>
+          <td class="output">${dateInput.value}</td>
+          <td class="output">${amountInput.value}</td>
+          <td><button class="delete_btn">"❌"</button></td>
+        </tr>`;
+  // let nameCell = document.createElement("td");
+  // let dateCell = document.createElement("td");
+  // let amountCell = document.createElement("td");
+  // let deleteCell = document.createElement("button");
+  // nameCell.appendChild(document.createTextNode(nameInput.value));
+  // dateCell.appendChild(document.createTextNode(dateInput.value));
+  // amountCell.appendChild(document.createTextNode(amountInput.value));
+  // deleteCell.appendChild(document.createTextNode("❌"));
+  // deleteCell.classList.add("delete_btn");
+  // let newRow = document.createElement("tr");
+  // newRow.appendChild(nameCell);
+  // newRow.appendChild(dateCell);
+  // newRow.appendChild(amountCell);
+  // newRow.appendChild(deleteCell);
+  expenseTable.insertAdjacentHTML("beforeend", newExpense);
 }
 
 function removeExpense(event) {
-  expenseTable.removeChild(event.target.parentElement);
+  if (event.target.className === "delete_btn")
+    event.target.parentNode.parentNode.remove();
 }
