8f56ea49b11162d9bd53675c1c37afde6f34f84e
Kaung722 <70074279+Kaung722@users.noreply.github.com>
2021-07-19 22:11:11 -0700

    added remove function

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const nameInput = document.querySelector("#nameInput");
 const dateInput = document.querySelector("#dateInput");
 const amountInput = document.querySelector("#amountInput");
 const addExpenseBtn = document.querySelector("#add_expense_button");
+const deleteBtn = document.querySelector(".delete_btn");
 const form = document.querySelector("#formID");
 let expenseTable = document.querySelector(".expense_table");
 
 //Listener
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", addExpense);
+expenseTable.addEventListener("click", removeExpense);
+
+//function
+function addExpense(event) {
   event.preventDefault();
   console.log(nameInput.value, dateInput.value, amountInput.value);
-
   let nameCell = document.createElement("td");
   let dateCell = document.createElement("td");
   let amountCell = document.createElement("td");
@@ -21,10 +25,15 @@ form.addEventListener("submit", function (event) {
   dateCell.appendChild(document.createTextNode(dateInput.value));
   amountCell.appendChild(document.createTextNode(amountInput.value));
   deleteCell.appendChild(document.createTextNode("❌"));
+  deleteCell.classList.add("delete_btn");
   let newRow = document.createElement("tr");
   newRow.appendChild(nameCell);
   newRow.appendChild(dateCell);
   newRow.appendChild(amountCell);
   newRow.appendChild(deleteCell);
   expenseTable.appendChild(newRow);
-});
+}
+
+function removeExpense(event) {
+  expenseTable.removeChild(event.target.parentElement);
+}
